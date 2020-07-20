8e06e4c40d5334c5cd8b33754b187ed092606561
geysa ladica <geysa.ladica@gmail.com>
2020-07-20 20:52:29 -0300

    create method details event

--- src/main/java/com/eventoapp/controller/EventoController.java
+++ src/main/java/com/eventoapp/controller/EventoController.java
@@ -4,6 +4,7 @@ import com.eventoapp.models.Evento;
 import com.eventoapp.repository.EventoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -34,5 +35,14 @@ public class EventoController {
         mv.addObject("eventos", eventos);
 
         return  mv;
+    }
+
+    @RequestMapping("/{codigo}")
+    public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo){
+        Evento evento = er.findByCdigo(codigo);
+        ModelAndView mv = new ModelAndView("detalhesEvento");
+        mv.addObject("evento", evento);
+
+        return mv;
     }
 }
